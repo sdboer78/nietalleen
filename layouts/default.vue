b8e9b585c55aa3e52234628a9204ba5f77995eef
Lennart Klein <lennart.klein@eo.nl>
2020-03-17 07:35:58 +0100

    redo tabbar navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
   <v-app>
+    <v-app-bar
+      app
+      color="white"
+      fixed
+    >
+      <v-tabs
+        v-model="activeTab"
+        color="primary"
+        grow
+      >
+        <v-tab v-for="tab in tabs" :key="tab.id" :to="tab.route" exact>
+          {{ tab.name }}
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
     <v-content>
       <nuxt />
     </v-content>
@@ -13,7 +28,8 @@ export default {
       activeTab: '/',
       tabs: [
         { id: 1, name: 'Ik zoek hulp', route: '/' },
-        { id: 2, name: 'Ik kan hulp bieden', route: '/hulp-bieden' }
+        { id: 2, name: 'Wij kunnen hulp bieden', route: '/hulp-bieden' },
+        { id: 3, name: '<v-icon>mdi-phone</v-icon>', route: '/meer-info' }
       ]
     }
   }
