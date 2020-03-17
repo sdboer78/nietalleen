2d59ee5a1c376a3b28700f9601af00ba14210906
Lennart Klein <lennart.klein@eo.nl>
2020-03-17 12:09:28 +0100

    make maps embed fullscreen

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,5 @@
 <template>
-  <v-layout
-    column
-    align-stretch
-    class="text-center"
-  >
-    <section class="grey lighten-2">
-      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1eDxpR0IUErARS5g1slGMmBlHpyDrPEbl" class="maps-iframe" />
-    </section>
-    <section class="white--text pa-8" style="background: linear-gradient(135deg, #C63866, #FE676E, #FD8F52);">
-      <v-container>
-        <h2 class="headline">
-          Wij laten ons niet inpakken door het corona-virus.
-        </h2>
-        <p>
-          Wij laten ons niet inpakken door het corona-virus. Dat kan onze gezondheid aanvallen, maar niet onze medemenselijkheid. Wij staan voor je klaar, overal in Nederland. Voor een belangstellend telefoongesprek of het regelen van praktische hulp. Je bent #nietalleen!
-        </p>
-      </v-container>
-    </section>
-  </v-layout>
+  <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1eDxpR0IUErARS5g1slGMmBlHpyDrPEbl" class="maps-iframe" />
 </template>
 
 <script>
@@ -37,7 +19,7 @@ export default {
 <style type="text/css" lang="scss">
 .maps-iframe {
   border: 0;
-  height: 80vh;
+  height: 100%;
   width: 100%;
 }
 </style>
