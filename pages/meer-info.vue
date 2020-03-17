c85edf815ff69e968d870aa53b7f98c38a078f1c
Lennart Klein <lennart.klein@eo.nl>
2020-03-17 07:36:15 +0100

    add page for information about the platform

---
+++ pages/meer-info.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout
+    column
+    align-stretch
+    class="text-center"
+  >
+    <v-container class="pa-8">
+      <h1>Meer informatie</h1>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
