77a752f12b4c3c9aa5bdd80d327076458de7f01c
Martijn Smit <martijnsmitt@gmail.com>
2020-03-17 13:23:02 +0100

    update the basic meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   */
   head: {
     titleTemplate: '%s #nietalleen',
-    title: process.env.npm_package_name || '',
+    title: '#nietalleen',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
