6aefbef8ab77037e720851841d7945dea2652b16
Martijn Smit <martijnsmitt@gmail.com>
2020-03-19 10:06:53 +0100

    update text on getting-help page

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s #nietalleen',
-    title: '#nietalleen',
+    titleTemplate: '%s #Nietalleen',
+    title: '#Nietalleen',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,28 @@
         </hero-image>
         <h1>Ik zoek hulp</h1>
         <need-help-form />
+        <p><br><br>of<br><br></p>
+        <p>Bel met ons nummer:</p>
+        <v-btn
+          href="tel:0800-1322"
+          color="primary"
+          x-large
+        >
+          <v-icon left>
+            mdi-phone
+          </v-icon>
+          0800-1322
+        </v-btn>
+        <p class="caption my-6">
+          <strong>maandag t/m donderdag:</strong>
+          van 09.00 - 21.00 uur
+          <br>
+          <strong>vrijdag:</strong>
+          van 09.00 - 16.00 uur
+        </p>
+        <p class="text-left pa-6">
+          Een lokale organisatie of kerk zal je zo snel mogelijk benaderen om je te helpen.
+        </p>
       </v-flex>
     </v-layout>
     <section>
@@ -38,79 +60,10 @@
             contain
             class="mx-auto mb-12"
           />
-          <h2>Ondanks het coronavirus (COVID-19), staan we voor je klaar.</h2>
+          <h2 class="display-1">Ondanks het coronavirus staan we voor je klaar.</h2>
           <p class="subtitle-1">
-            Overal in Nederland zetten talloze lokale organisaties en kerken zich in voor mensen die hulp kunnen gebruiken. Praktische hulp nodig? Een luisterend oor? Je bent #nietalleen.
+            Ondanks het coronavirus (COVID-19), staan we voor je klaar. Overal in Nederland zetten talloze lokale organisaties en kerken zich in voor mensen die hulp kunnen gebruiken. Praktische hulp nodig? Een luisterend oor? Je bent #Nietalleen.
           </p>
-        </v-flex>
-      </v-layout>
-    </section>
-    <section>
-      <v-layout
-        justify-center
-        align-center
-        class="px-8 py-12 text-center"
-      >
-        <v-flex xs12 sm9 md5>
-          <h2>Zo werkt het</h2>
-          <v-row>
-            <v-col
-              cols="12"
-              sm="5"
-              class="text-sm-left"
-            >
-              <p>Bel met ons nummer:</p>
-              <v-btn href="tel:0800-1322" color="primary">
-                <v-icon left>
-                  mdi-phone
-                </v-icon>
-                0800-1322
-              </v-btn>
-              <p class="caption mb-3 mt-6">
-                <strong>maandag t/m donderdag:</strong>
-                van 09.00 - 21.00 uur
-              </p>
-              <p class="caption">
-                <strong>vrijdag:</strong>
-                van 09.00 - 16.00 uur
-              </p>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="2"
-              class="align-self-center"
-            >
-              <v-row class="text-center flex-column">
-                <v-col class="d-sm-none">
-                  <hr class="my-3">
-                </v-col>
-                <v-col cols="2">
-                  OF
-                </v-col>
-                <v-col class="d-sm-none">
-                  <hr class="my-3">
-                </v-col>
-              </v-row>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="5"
-              class="text-sm-left"
-            >
-              <p>E-mail ons:</p>
-              <v-btn href="mailto:info@nietalleen.nl" color="primary">
-                <v-icon left>
-                  mdi-mail
-                </v-icon>
-                info@nietalleen.nl
-              </v-btn>
-              <p class="caption mb-3 mt-6">
-                <strong>Geef aan ons door:</strong>
-                Naam, postcode e-mailadres en hulpvraag.
-              </p>
-            </v-col>
-          </v-row>
-          <p><br>Een lokale organisatie of kerk zal je zo snel mogelijk benaderen om je te helpen.</p>
         </v-flex>
       </v-layout>
     </section>
@@ -121,17 +74,58 @@
         class="text-center accent px-8 py-12"
       >
         <v-flex xs12 sm8 md5 class="my-5">
-          <h2>Wil jij je aansluiten met een hulp-initiatief?</h2>
+          <span class="display-4">133</span>
+          <h2 class="display-1">organisaties hebben al hulp aangeboden</h2>
+          <p>Wil jij je ook aansluiten met een hulp-initiatief?</p>
           <v-btn
             href="https://forms.office.com/Pages/ResponsePage.aspx?id=Ra6c-sHLkUCQ_b7C2EYL3rpiNnpU7XZOndQiob_pkfJUMzU3VUw1WFA0WVJZU1pLUk9PVUFLT0NHMy4u"
             target="_new"
             color="primary"
+            x-large
+            class="mt-6"
           >
             Aanmelden
           </v-btn>
-          <p class="caption mt-5">
-            Ben je <strong>een particulier</strong> en wil je ook graag helpen? We gaan binnenkort op de website laten zien bij welke organisatie bij jou in de buurt je kan aansluiten.
+          <p class="body-2 font-weight-bold pa-12">
+            Ben je een particulier en wil je ook graag helpen? We gaan binnenkort op de website laten zien bij welke organisatie bij jou in de buurt je kan aansluiten.
           </p>
+        </v-flex>
+      </v-layout>
+    </section>
+    <section>
+      <v-layout
+        justify-center
+        align-center
+        class="text-center px-8 py-12"
+      >
+        <v-flex xs12 sm8 md5 class="my-5">
+          <hero-image
+            :src="require('@/assets/hond-uitlaten.jpg')"
+            size="400px"
+            class="mx-auto mb-8"
+          >
+            <v-img
+              :src="require('@/assets/logo-niet-alleen.svg')"
+              max-width="200px"
+              aspect-ratio="5"
+              position="center center"
+              contain
+            />
+          </hero-image>
+          <h2 class="display-1">Over ons</h2>
+          <p>
+            Met #Nietalleen verbinden we de talloze lokale en kerkelijke hulpinitiatieven aan elkaar en aan hulpvragers.
+            <br>De hulpvraag wordt door één van de samenwerkende organisaties opgepakt en doorgestuurd
+          </p>
+          <v-btn
+            href="/over"
+            color="primary"
+            class="mt-6"
+            x-large
+            nuxt
+          >
+            Meer lezen over ons
+          </v-btn>
         </v-flex>
       </v-layout>
     </section>
