99d38545f19f512c0f7f26e81feda4ce73a4b94c
Lennart Klein <lennart.klein@eo.nl>
2020-03-20 13:59:26 +0100

    add information about donating to the application

---
+++ pages/doneren.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <section>
+      <v-layout
+        justify-center
+        align-center
+        class="pa-8 text-center"
+      >
+        <v-flex xs12 sm8 md6 lg4 xl3 class="pb-12">
+          <hero-image
+            :src="require('@/assets/vegetable-basket.jpg')"
+            size="400px"
+            class="mx-auto mb-8"
+          >
+            <v-img
+              :src="require('@/assets/logo-niet-alleen.svg')"
+              max-width="200px"
+              aspect-ratio="5"
+              position="center center"
+              contain
+            />
+          </hero-image>
+          <h1 class="display-1">
+            Corona raakt ons allemaal: blijf om elkaar geven
+          </h1>
+          <p class="mt-10">
+            Van Apeldoorn tot Afghanistan en van Rotterdam tot Rwanda: de gevolgen van het coronavirus raken ons allemaal. Dichtbij, in onze huizen en onze steden. Verder weg, bij onze medemensen over de hele wereld.
+          </p>
+          <p>
+            Daarom start EO Metterdaad een noodhulpcampagne om u de mogelijkheid te geven ook financieel te helpen. Dat doen we samen met de initiatiefnemers achter nietalleen.nl en met de andere hulporganisaties in ons netwerk. Zij doen wat ze kunnen om mensen in nood te bij te staan. In Nederland én wereldwijd. Met uw gift steunt u hun belangrijke werk.
+          </p>
+          <p>
+            Zoals altijd zien wij er zorgvuldig op toe dat donaties terecht komen waar ze nodig zijn. Dat doen we als EO Metterdaad immers al bijna 50 jaar. Geef dus met een gerust hart.
+          </p>
+          <v-img
+            :src="require('@/assets/logo-anbi.svg')"
+            class="mx-auto my-5"
+            max-width="40px"
+            aspect-ratio="1.27"
+            position="center center"
+            contain
+          />
+          <v-btn
+            href="https://www.metterdaad.nl/corona/doneren"
+            color="primary"
+            class="ma-2"
+            nuxt
+            x-large
+          >
+            Doneer
+            <v-icon right>mdi-open-in-new</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </section>
+  </div>
+</template>
+
+<script>
+import HeroImage from '~/components/HeroImage.vue'
+
+export default {
+  components: { HeroImage },
+  head () {
+    return {
+      title: 'Doneren',
+      meta: [
+        { hid: 'doneren', name: 'description', content: 'EO Metterdaad heeft een noodhulpcampagne om u de mogelijkheid te geven ook financieel te helpen. Met uw gift steunt u het belangrijke werk van de de initiatiefnemers achter nietalleen.nl en andere hulporganisaties.' }
+      ]
+    }
+  }
+}
+</script>
