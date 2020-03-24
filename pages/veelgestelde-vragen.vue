85a6d5ffe121e1cd1aca31545cae1f9b1e68952f
Martijn Smit <martijnsmitt@gmail.com>
2020-03-24 16:29:16 +0100

    add real static faq data

--- pages/veelgestelde-vragen.vue
+++ pages/veelgestelde-vragen.vue
@@ -37,7 +37,7 @@
                 {{ item.question }}
               </v-expansion-panel-header>
               <v-expansion-panel-content>
-                {{ item.answer }}
+                <span v-html="item.answer"></span>
               </v-expansion-panel-content>
             </v-expansion-panel>
             <!-- repeat -->
@@ -57,12 +57,40 @@ export default {
       openPanels: [],
       faq: [
         {
-          question: 'Dit is mijn vraag',
-          answer: 'Dit is mijn antwoord'
+          question: 'Wat is #Nietalleen?',
+          answer: 'Een online website en platform waar hulpvragen van particulieren en aanbod van diensten door kerken en lokale organisaties bij elkaar gebracht worden.'
         },
         {
-          question: 'Dit is mijn vraag',
-          answer: 'Dit is mijn tweede antwoord'
+          question: 'Waarom is #Nietalleen opgericht?',
+          answer: 'Kerken en lokale (hulp)organisaties bundelen de krachten, zodat overal in het land praktische en betrouwbare hulp geboden kan worden en de initiatieven elkaar kunnen versterken. Een hulpvraag wordt opgepakt en doorgestuurd naar een organisatie bij de hulpvrager in de buurt'
+        },
+        {
+          question: 'Hoe kunnen mensen om hulp vragen? ',
+          answer: 'Door te bellen met <a href="tel:0800-1322">0800-1322</a>'
+        },
+        {
+          question: 'Wat kunnen mensen verwachten van #Nietalleen?',
+          answer: 'Het gaat om praktische hulp én een luisterend oor. Dus om boodschappen doen of medicijnen halen, honden uitlaten, belrondes onder oudere gemeenteleden doen en voedsel inzamelen voor de voedselbank.'
+        },
+        {
+          question: 'Hoe kunnen kerken en lokale organisaties hun diensten aanbieden?',
+          answer: 'Kerken en lokale organisaties kunnen zichzelf aanmelden op <a href="https://nietalleen.nl/hulp-bieden">https://nietalleen.nl/hulp-bieden</a> door het formulier in te vullen.'
+        },
+        {
+          question: 'Kunnen particulieren ook hun diensten aanbieden?',
+          answer: 'Binnenkort kunnen ook particulieren hun hulpaanbod hier aangeven, dan is op <a href="https://nietalleen.nl/">https://nietalleen.nl/</a> te zien bij welke organisatie in de buurt er aansluiting gemaakt kan worden. '
+        },
+        {
+          question: 'Voor wie is #Nietalleen bedoeld?',
+          answer: 'Voor iedereen die behoefte heeft aan een gesprek of hulp, ongeacht leeftijd of situatie. Dankzij ons landelijk dekkende netwerk kunnen we overal in Nederland hulp en aanbod verbinden.'
+        },
+        {
+          question: 'Welke kerken en organisaties zitten er achter #Nietalleen',
+          answer: 'Christelijk Gereformeerde Kerken<br>ChristenUnie<br>Diaconaal Steunpunt<br>Evangelische Omroep<br>Gereformeerde Kerken Vrijgemaakt<br>Kerk in Actie<br>Missie Nederland<br>Navigators<br>Nederlands Gereformeerde Kerken<br>Protestantse Kerk in Nederland<br>Rooms Katholieke Kerk<br>SGP<br>Stichting Agapè<br>Hip Helpt<br>Stichting Present<br>Stichting Schuldhulpmaatje<br>Tear<br>Tot Heil des Volks<br>4e Musketier<br>Youth for Christ<br>En nog steeds breidt de lijst zich uit… kijk op <a href="https://nietalleen.nl/over/">https://nietalleen.nl/over/</a> voor de meest recente lijst.<br>PKN en EO coördineren de onderlinge samenwerking.'
+        },
+        {
+          question: 'Wordt mijn hulpvraag altijd opgepakt?',
+          answer: 'Met het platform #Nietalleen probeert de EO zo goed mogelijk een hulpvraag aan de juiste hulpaanbieder te binden. Daarmee is #Nietalleen verantwoordelijk voor het doorzetten van de hulpvraag naar (lokale) initiatieven. Het indienen van een hulpvraag betekent niet dat er recht op hulp ontstaat; de (lokale) aanbieders zullen hun best doen om jouw vraag zo goed mogelijk op te pakken. De kwaliteit, in algemene zin, van het hulpaanbod ligt bij de initiatieven zelf.'
         }
       ]
     }
