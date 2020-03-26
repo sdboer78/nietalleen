ff2b8c7aff8455edaaff8db30c577eef2dd4ee98
Lennart Klein <lennart.klein@eo.nl>
2020-03-26 11:02:52 +0100

    make /verhalen look more like an interactive piece withgrey background

--- pages/verhalen.vue
+++ pages/verhalen.vue
@@ -1,44 +1,42 @@
 <template>
-  <div>
-    <section class="pa-8 text-center">
-      <v-layout
-        justify-center
-        align-center
+  <section class="pa-8 accent fill-height text-center">
+    <v-layout
+      justify-center
+      align-center
+    >
+      <v-flex
+        xs12
+        sm8
+        md6
+        lg4
+        xl3
       >
-        <v-flex
-          xs12
-          sm8
-          md6
-          lg4
-          xl3
-        >
-          <h1 class="display-1 mb-2">
-            #Nietalleen verhalen
-          </h1>
-          <p class="font-weight-bold mb-8">
-            Inspireer elkaar. Deel jouw verhaal met #Nietalleen op Twitter, Facebook of Instagram.
-          </p>
-        </v-flex>
-      </v-layout>
-      <v-layout
-        justify-center
-        align-center
+        <h1 class="display-1 mb-2">
+          #Nietalleen verhalen
+        </h1>
+        <p class="font-weight-bold mb-8">
+          Inspireer elkaar. Deel jouw verhaal met #Nietalleen op Twitter, Facebook of Instagram.
+        </p>
+      </v-flex>
+    </v-layout>
+    <v-layout
+      justify-center
+      align-center
+    >
+      <v-flex
+        xs12
+        sm12
+        md12
+        lg12
+        xl12
       >
-        <v-flex
-          xs12
-          sm12
-          md12
-          lg12
-          xl12
-        >
-          <social-wall
-            :render-wall="allowSocialwallRender"
-            class="mx-n2"
-          />
-        </v-flex>
-      </v-layout>
-    </section>
-  </div>
+        <social-wall
+          :render-wall="allowSocialwallRender"
+          class="mx-n2"
+        />
+      </v-flex>
+    </v-layout>
+  </section>
 </template>
 
 <script>
