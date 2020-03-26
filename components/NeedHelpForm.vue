3ae67b837eea1736f6c438849107af6e51c48eaa
Lennart Klein <lennart.klein@eo.nl>
2020-03-26 10:19:12 +0100

    hide a text-field until it is booted

--- components/NeedHelpForm.vue
+++ components/NeedHelpForm.vue
@@ -342,7 +342,12 @@ export default {
 
 <style type="text/css" lang="scss" scoped>
   .v-text-field {
-    position: relative;
+    opacity: 0;
+    transition: opacity 0.2s cubic-bezier(0.4, 0.0, 0.2, 1);
+
+    &--is-booted {
+      opacity: 1;
+    }
   }
 
   .v-text-field .v-text-field__suffix {
